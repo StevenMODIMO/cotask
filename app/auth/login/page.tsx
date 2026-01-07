23cb94f30d51f89388cdf42523dd4c6102df6134
StevenMODIMO <stevepetes26@gmail.com>
2026-01-07 16:24:50 +0300

    Cotask version 1

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,24 @@
 import type { Metadata } from "next";
 import LoginForm from "@/components/login-form";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-export default function Login() {
-  return <div>
-    <LoginForm />
-  </div>;
+export default async function Login() {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getClaims();
+
+  const user = data?.claims;
+
+  if(user) {
+    redirect("/dashboard")
+  }
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 }
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,10 +1,24 @@
 import type { Metadata } from "next";
 import SignupForm from "@/components/signup-form";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Create Account",
 };
 
-export default function Signup() {
-  return <div><SignupForm /></div>;
+export default async function Signup() {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getClaims();
+
+  const user = data?.claims;
+
+  if (user) {
+    redirect("/dashboard");
+  }
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
 }
