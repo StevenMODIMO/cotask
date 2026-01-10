b69cd8c3cb47413ba8eeef391493a409b61955aa
StevenMODIMO <stevepetes26@gmail.com>
2026-01-11 00:18:55 +0300

    Cotask version 1

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -165,7 +165,7 @@ function NavItem({
     <Link
       href={href}
       className="
-        flex flex-col items-center gap-1 hover:bg-muted lg:p-[2px]
+        flex flex-col items-center gap-1 hover:bg-muted lg:p-0.5
         md:flex-row md:gap-3
       "
     >
