036ec0392aebc710b01e7b3f1943cebd55223c77
StevenMODIMO <stevepetes26@gmail.com>
2026-01-16 18:09:36 +0300

    cotask version 1

--- app/dashboard/tasks/components/header.tsx
+++ app/dashboard/tasks/components/header.tsx
@@ -33,27 +33,45 @@ export default function TaskHeader({ id }: { id: string }) {
 
   return (
     <nav className="border-r-2 h-screen flex flex-col gap-6 px-2">
-      <PanelTopClose
-        onClick={toggleView}
-        className={`${
-          expanded ? "rotate-90" : "rotate-270"
-        } transition-transform duration-500`}
-        size={20}
-      />
-      <Link href={`/dashboard/tasks`}>
+      <div onClick={toggleView} className="hidden md:flex items-center gap-2">
+        <PanelTopClose
+          className={`${
+            !expanded ? "rotate-90" : "rotate-270"
+          } transition-transform duration-500`}
+          size={20}
+        />
+        {expanded && <span>Collapse</span>}
+      </div>
+      <Link href={`/dashboard/tasks`} className="flex items-center gap-2">
         <House size={20} />
+        <span className={`hidden ${expanded ? "md:block" : "hidden"}`}>
+          Back to tasks
+        </span>
       </Link>
-      <Link href={`/dashboard/tasks/${id}`}>
+      <Link href={`/dashboard/tasks/${id}`} className="flex items-center gap-2">
         <StickyNote size={20} />
+        <span className={`hidden ${expanded ? "md:block" : "hidden"}`}>Overview</span>
       </Link>
-      <Link href={`/dashboard/tasks/${id}/edit`}>
+      <Link
+        href={`/dashboard/tasks/${id}/edit`}
+        className="flex items-center gap-2"
+      >
         <Pencil size={20} />
+        <span className={`hidden ${expanded ? "md:block" : "hidden"}`}>Update task</span>
       </Link>
-      <Link href={`/dashboard/tasks/${id}/delete`}>
+      <Link
+        href={`/dashboard/tasks/${id}/delete`}
+        className="flex items-center gap-2"
+      >
         <Trash size={20} />
+        <span className={`hidden ${expanded ? "md:block" : "hidden"}`}>Delete task</span>
       </Link>
-      <Link href={`/dashboard/tasks/${id}/settings`}>
+      <Link
+        href={`/dashboard/tasks/${id}/settings`}
+        className="flex items-center gap-2"
+      >
         <Bolt size={20} />
+        <span className={`hidden ${expanded ? "md:block" : "hidden"}`}>Settings</span>
       </Link>
     </nav>
   );
